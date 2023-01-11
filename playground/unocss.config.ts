7d9b565c92f885cd77c392f1d765672673e9f631
deuscx <1475453714@qq.com>
2023-01-11 21:17:33 +0800

    feat: add playground

---
+++ playground/unocss.config.ts
@@ -0,0 +1,29 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetUno,
+  transformerCompileClass,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss'
+import { presetDaisy } from '@deuscx/unocss-preset-daisy'
+export default defineConfig({
+  theme: {
+    fontFamily: {
+      sans: '\'Inter\', sans-serif',
+      mono: '\'Fira Code\', monospace',
+    },
+  },
+  presets: [
+    presetAttributify(),
+    presetUno(),
+    presetIcons(),
+    presetDaisy(),
+  ],
+  transformers: [
+    transformerCompileClass(),
+    transformerVariantGroup(),
+    transformerDirectives(),
+  ],
+})
